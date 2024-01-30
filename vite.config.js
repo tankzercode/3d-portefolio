c4f9a955e0425ba24cd68069e5012dd628c4281c
tanguy <tlegalldutertre@gmail.com>
2024-01-31 00:13:33 +0100

    lkj

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: "build",
+  },
+  base: './',  
+
+  plugins: [ react()],
+});
